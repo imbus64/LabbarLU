601b3c92871352ca4ce21edf542356a69a5d715f
Imbus <imbus64@protonmail.com>
2023-10-07 10:50:33 +0200

    Implement Comparator for Signal and give it a proper constructor. Make SignalList use a prioqueue (max heap)
    Former-commit-id: 1bc405d148654508e567224462cea54e10be96ff

--- EITG01-Lab2/src/main/java/labs/Signal.java
+++ EITG01-Lab2/src/main/java/labs/Signal.java
@@ -1,10 +1,23 @@
 package labs;
 
-// Denna klass definerar vad som ska finnas i en signal. Det som finns h�r �r ett minimum. Man kan l�gga till mer
+// Denna klass definerar vad som ska finnas i en signal. Det som finns här är ett minimum. Man kan lägga till mer
 // om man vill att en signal ska kunna skicka mer information.
-class Signal {
+class Signal implements Comparable<Signal> {
 	public Proc destination;
 	public double arrivalTime;
 	public int signalType;
 	public Signal next;
+
+	Signal(int type, Proc dest, double time) {
+		signalType = type;
+		destination = dest;
+		arrivalTime = time;
+		next = null;
+	}
+
+	// Tells various Collection classes how to sort Signals
+	@Override
+	public int compareTo(Signal other) {
+		return Double.compare(arrivalTime, other.arrivalTime);
+	}
 }
--- EITG01-Lab2/src/main/java/labs/SignalList.java
+++ EITG01-Lab2/src/main/java/labs/SignalList.java
@@ -1,37 +1,17 @@
 package labs;
 
-// Denna klass definierar signallistan. Om man vill skicka mer information i signalen �n minimum, s� kan
-// man skriva ytterligare variante av SendSignal som inneh�ller fler parametrar.
-public class SignalList {
-	public static Signal list, last;
+import java.util.PriorityQueue;
 
-	SignalList() {
-		list = new Signal();
-		last = new Signal();
-		list.next = last;
-	}
+// Static data members are a crime against humanity
+public class SignalList {
+	// See comparator in Signal.java
+	static PriorityQueue<Signal> list = new PriorityQueue<>();
 
 	public static void SendSignal(int type, Proc dest, double arrtime) {
-		Signal dummy, predummy;
-		Signal newSignal = new Signal();
-		newSignal.signalType = type;
-		newSignal.destination = dest;
-		newSignal.arrivalTime = arrtime;
-		predummy = list;
-		dummy = list.next;
-		while ((dummy.arrivalTime < newSignal.arrivalTime) & (dummy != last)) {
-			predummy = dummy;
-			dummy = dummy.next;
-		}
-		predummy.next = newSignal;
-		newSignal.next = dummy;
+		list.add(new Signal(type, dest, arrtime));
 	}
 
-	public static Signal FetchSignal() {
-		Signal dummy;
-		dummy = list.next;
-		list.next = dummy.next;
-		dummy.next = null;
-		return dummy;
+	static public Signal FetchSignal() {
+		return list.poll();
 	}
 }
