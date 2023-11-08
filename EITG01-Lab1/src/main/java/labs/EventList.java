40cf6ea63e1275c33d1d011c444ad5e3376ecfa0
Imbus <imbus64@protonmail.com>
2023-11-08 13:43:40 +0100

    Lab1 formatting
    Former-commit-id: d3669b9d7dbc47f68ebf11ddd9820a90ab748744

--- EITG01-Lab1/src/main/java/labs/EventList.java
+++ EITG01-Lab1/src/main/java/labs/EventList.java
@@ -1,32 +1,32 @@
 package labs;
 
-public class EventList{
-	
+public class EventList {
+
 	private Event list, last;
-	
-	EventList(){
+
+	EventList() {
 		list = new Event();
-    	last = new Event();
-    	list.next = last;
+		last = new Event();
+		list.next = last;
 	}
-	
-	public void InsertEvent(int type, double TimeOfEvent){
- 	Event dummy, predummy;
- 	Event newEvent = new Event();
-	// newEvent.subEventType = subType;
- 	newEvent.eventType = type;
- 	newEvent.eventTime = TimeOfEvent;
- 	predummy = list;
- 	dummy = list.next;
- 	while ((dummy.eventTime < newEvent.eventTime) & (dummy != last)){
- 		predummy = dummy;
- 		dummy = dummy.next;
- 	}
- 	predummy.next = newEvent;
- 	newEvent.next = dummy;
- }
-	
-	public Event FetchEvent(){
+
+	public void InsertEvent(int type, double TimeOfEvent) {
+		Event dummy, predummy;
+		Event newEvent = new Event();
+		// newEvent.subEventType = subType;
+		newEvent.eventType = type;
+		newEvent.eventTime = TimeOfEvent;
+		predummy = list;
+		dummy = list.next;
+		while ((dummy.eventTime < newEvent.eventTime) & (dummy != last)) {
+			predummy = dummy;
+			dummy = dummy.next;
+		}
+		predummy.next = newEvent;
+		newEvent.next = dummy;
+	}
+
+	public Event FetchEvent() {
 		Event dummy;
 		dummy = list.next;
 		list.next = dummy.next;
