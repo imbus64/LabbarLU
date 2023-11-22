c4a9ead1a34f433e3d81e203a34153e68166601c
Imbus <imbus64@protonmail.com>
2023-11-22 22:15:38 +0100

    Clean EDAA30 Lab 5
    Former-commit-id: b676551c65285b0c100c1561f5007a16d8cd23bc

---
+++ EDAA30-Lab5/src/test/java/labs/AppTest.java
@@ -0,0 +1,13 @@
+package labs;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+
+class AppTest {
+    @Disabled
+    @Test void appHasAGreeting() {
+        assertNull(null);
+    }
+}
