032650b7c013088517a88b7ad46da45b429bc372
Imbus <imbus64@protonmail.com>
2023-12-05 12:21:13 +0100

    Test cases

--- EDAA30-Lab6/src/test/java/labs/BinarySearchTreeTest.java
+++ EDAA30-Lab6/src/test/java/labs/BinarySearchTreeTest.java
@@ -3,12 +3,27 @@ package labs;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Comparator;
+import java.util.Random;
 
 import org.junit.jupiter.api.Test;
 
 import bst.BinarySearchTree;
 
 class BinarySearchTreeTest {
+    @Test
+    void testRebuilding() {
+        for (int i = 0; i < 10; i++) {
+            BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+            for (int j = 0; j < 50; j++) {
+                bst.add(j);
+            }
+
+            // Rebuild the tree
+            bst.rebuild();
+            assertTrue(bst.isBalanced());
+            assertEquals(bst.size(), 50);
+        }
+    }
 
     @Test
     void testAdd() {
@@ -23,16 +38,12 @@ class BinarySearchTreeTest {
     void testHeight() {
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
         assertEquals(0, bst.height());
-
         bst.add(5);
         assertEquals(1, bst.height());
-
         bst.add(3);
         assertEquals(2, bst.height());
-
         bst.add(8);
         assertEquals(2, bst.height());
-
         bst.add(2);
         assertEquals(3, bst.height());
     }
@@ -41,13 +52,8 @@ class BinarySearchTreeTest {
     void testSize() {
         BinarySearchTree<String> bst = new BinarySearchTree<>();
         assertEquals(0, bst.size());
-
         bst.add("apple");
-        assertEquals(1, bst.size());
-
         bst.add("banana");
-        assertEquals(2, bst.size());
-
         bst.add("cherry");
         assertEquals(3, bst.size());
     }
@@ -79,4 +85,18 @@ class BinarySearchTreeTest {
         assertEquals(3, bst2.size());
     }
 
+    @Test
+    void stressTest() {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 50; j++) {
+                bst.add(rand.nextInt(100) + 1);
+            }
+            assert(bst.size() > 0);
+            bst.clear();
+            assertEquals(0, bst.size());
+        }
+    }
 }
